a562c611cbc1f541d7dbafae9d78f3d3ce1dfde4
Marcus Nowotny <marcus@interactive-matter.org>
2010-08-11 18:30:42 +0200

    Now it is a real Arduino test project - still too big

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <WProgram.h>
+
+// The setup() method runs once, when the sketch starts
+void setup()   {
+}
+
+// the loop() method runs over and over again,
+// as long as the Arduino has power
+
+void loop()
+{
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
